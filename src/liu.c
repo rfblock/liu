d1c9491606c316c743d59f9d2481b4adeff55e25
Steggy <steggy@steg.gy>
2025-05-14 03:41:13 -0400

    Logging refactor

--- src/liu.c
+++ src/liu.c
@@ -12,6 +12,7 @@
 	#define access _access
 #else
 	#include <unistd.h>
+	#include <errno.h>
 #endif
 
 #ifdef ENABLE_COLORS
@@ -45,6 +46,32 @@
 	#define TEXT_FATAL "[fatal] "
 	#define TEXT_TODO "[todo] "
 #endif
+
+#define LIU_HIGHLIGHT(x) ANSI_BOLD ANSI_UNDERLINE x ANSI_RESET
+
+#define PRINTF_WARNING(fmt, ...) printf(TEXT_WARNING fmt, ##__VA_ARGS__)
+#define PRINTLN_WARNING(x) PRINTF_WARNING("%s\n", x);
+
+#define PRINTF_FATAL(fmt, ...) do { printf(TEXT_FATAL fmt, ##__VA_ARGS__); exit(-1); } while (0)
+#define PRINTLN_FATAL(x) PRINTF_FATAL("%s\n", x);
+
+#define LIU_ASSERT(cond, msg) do { \
+	if (!(cond)) { \
+		PRINTLN_FATAL(msg)	\
+	} \
+} while (0)
+
+#define LIU_ASSERTF(cond, msg, ...) do { \
+	if (!(cond)) { \
+		PRINTF_FATAL(msg, ##__VA_ARGS__); \
+	} \
+} while (0)
+
+#define LIU_PRINTF_TRACELOG(fmt, ...) do { \
+	if (liu_config_enable_tracelog) { \
+		printf(fmt "\n", ##__VA_ARGS__); \
+	} \
+} while (0)
 
 char *liu_config_source_dir = NULL;
 char *liu_config_object_dir = NULL;
@@ -81,7 +108,7 @@ void liu_generate_liu_file(void) {
 	char file[256];
 	
 	if (access(".liu", F_OK) == 0) {
-		printf(TEXT_WARNING ".liu file already exists, this will overwrite it\n");
+		PRINTLN_WARNING(".liu file already exists, this will overwrite it");
 	}
 
 	printf("Enter output binary name (eg. my_program): " ANSI_GREEN);
@@ -92,10 +119,7 @@ void liu_generate_liu_file(void) {
 	#endif
 
 	FILE *fp = fopen(".liu", "w");
-	if (fp == NULL) {
-		printf(TEXT_FATAL " Could not create .liu file\n");
-		exit(-1);
-	}
+	LIU_ASSERT(fp != NULL, "Could not create .liu file");
 
 	char *format=(
 		"BINARY_NAME %s\n"
@@ -106,10 +130,7 @@ void liu_generate_liu_file(void) {
 	int size = snprintf(NULL, 0, format, file);
 	char *out = malloc(size + 1);
 
-	if (out == NULL) {
-		printf(TEXT_FATAL " Could not generate .liu file content\n");
-		exit(-1);
-	}
+	LIU_ASSERT(out != NULL, "Could not generate .liu file content");
 
 	snprintf(out, size + 1, format, file);
 
@@ -123,10 +144,7 @@ void liu_generate_liu_file(void) {
 void liu_scan_liu_file(void)
 {
 	FILE *fp = fopen(".liu", "rb");
-	if (fp == NULL) {
-		printf(TEXT_FATAL "Could not read .liu file\n");
-		exit(-1);
-	}
+	LIU_ASSERT(fp != NULL, "Could not read .liu file");
 
 	while (1) {
 		int size;
@@ -139,17 +157,11 @@ void liu_scan_liu_file(void)
 		}
 
 		char *key = malloc(size + 1);
-		if (key == NULL) {
-			printf(TEXT_FATAL "Unable to allocate key\n");
-			exit(-1);
-		}
+		LIU_ASSERT(key != NULL, "Unable to allocate key");
 
-		if (fseek(fp, -size, SEEK_CUR)) {
-			printf(TEXT_FATAL "Could not seek to scan key\n");
-			exit(-1);
-		}
+		LIU_ASSERT(!fseek(fp, -size, SEEK_CUR), "Could not seek to scan key");
 		if (fscanf(fp, "%s", key) == EOF) {
-			printf(TEXT_WARNING "Sanity check failed, could not scan key\n");
+			PRINTLN_WARNING("Sanity check failed, could not scan key");
 			free(key);
 			break;
 		}
@@ -175,10 +187,7 @@ void liu_scan_liu_file(void)
 			continue;
 		}
 
-		if (*value != NULL) {
-			printf(TEXT_FATAL "Redefinition of " ANSI_RED ANSI_BOLD ANSI_UNDERLINE "%s" ANSI_RESET "\n", key);
-			exit(-1);
-		}
+		LIU_ASSERTF(*value == NULL, "Redefinition of " ANSI_RED LIU_HIGHLIGHT("%s") "\n", key);
 
 		free(key);
 		key = NULL;
@@ -188,14 +197,11 @@ void liu_scan_liu_file(void)
 		}
 
 		*value = malloc(size + 1);
-		if (*value == NULL) {
-			printf(TEXT_FATAL "Unable to allocate value\n");
-			exit(-1);
-		}
+		LIU_ASSERT(*value != NULL, "Unable to allocate value\n");
 
 		fseek(fp, -size, SEEK_CUR);
 		if(fscanf(fp, " %[^\n]", *value) == EOF) {
-			printf(TEXT_WARNING "Sanity check failed, unable to scan value\n");
+			PRINTLN_WARNING("Sanity check failed, unable to scan value");
 			break;
 		}
 	}
@@ -234,10 +240,7 @@ void liu_run_tests(void)
 void liu_add_object_path(const char *path)
 {
 	size_t len = strlen(path);
-	if (object_paths_size + len + 2 >= sizeof object_paths) {
-		printf(TEXT_FATAL "Length of object paths exceeds buffer size\n");
-		exit(-1);
-	}
+	LIU_ASSERT(object_paths_size + len + 2 < sizeof object_paths, "Length of object paths exceeds buffer size");
 
 	if (object_paths_size == 0) {
 		object_paths[0] = '\0';
@@ -254,38 +257,34 @@ void liu_add_object_path(const char *path)
 
 void liu_mkdir_silent(const char *path)
 {
+	int print;
 #ifdef _WIN32
 	mkdir(path);
+	print = true; // TODO: Print only when created
 #else
 	mkdir(path, 755);
+	print = errno != EEXIST;
 #endif
-	if (liu_config_enable_tracelog) {
-		printf("mkdir %s\n", path);
+	if (print) {
+		LIU_PRINTF_TRACELOG("mkdir %s", path);
 	}
 }
 
 void liu_rename_file(const char *from, const char *to)
 {
-	if (liu_config_enable_tracelog) {
-		printf("rename %s %s\n", from, to);
-	}
+	LIU_PRINTF_TRACELOG("rename %s %s", from, to);
 	rename(from, to);
 }
 
 void liu_remove_file(const char *path)
 {
-	if (liu_config_enable_tracelog) {
-		printf("remove %s\n", path);
-	}
+	LIU_PRINTF_TRACELOG("remove %s", path);
 	remove(path);
 }
 
 void liu_remove_dir(const char *path)
 {
-	if (liu_config_enable_tracelog) {
-		printf("remove %s\n", path);
-	}
-
+	LIU_PRINTF_TRACELOG("remove %s", path);
 	rmdir(path);
 }
 
@@ -361,11 +360,8 @@ int liu_build_file(const char *path, const struct stat *, int typeflag, struct F
 	// TODO: multi-core this
 	char buf[BUFSIZ];
 	int ret = snprintf(buf, sizeof buf, "%s -o %s/%s -c %s %s", liu_config_compiler_binary, liu_config_object_dir, obj_name, liu_config_compiler_flags, path);
-	if (ret < 0) {
-		printf(TEXT_FATAL "Could not compile object\n");
-		exit(-1);
-	}
-	if (liu_config_enable_tracelog) { printf("%s\n", buf); }
+	LIU_ASSERT(ret >= 0, "Could not compile object");
+	LIU_PRINTF_TRACELOG("%s", buf);
 	system(buf);
 
 	free(obj_name);
@@ -375,7 +371,6 @@ int liu_build_file(const char *path, const struct stat *, int typeflag, struct F
 
 void liu_build_all(char *dirname)
 {
-	printf("BEGIN BUILD\n");
 	liu_mkdir_silent(liu_config_object_dir);
 	
 	nftw(dirname, &liu_build_file, 8, FTW_PHYS);
@@ -384,11 +379,8 @@ void liu_build_all(char *dirname)
 
 	char buf[BUFSIZ];
 	int ret = snprintf(buf, sizeof buf, "%s -o %s%s %s", liu_config_compiler_binary, binary_prefix, liu_config_binary_name, object_paths);
-	if (liu_config_enable_tracelog) { printf("%s\n", buf); }
-	if (ret < 0) {
-		printf(TEXT_FATAL "Could not link files\n");
-		exit(-1);
-	}
+	LIU_PRINTF_TRACELOG("%s", buf);
+	LIU_ASSERT(ret >= 0, "Could not link files");
 	system(buf);
 
 	if (liu_config_replace_binary) {
@@ -403,9 +395,9 @@ void liu_build_all(char *dirname)
 
 void clean(void)
 {
-	printf(TEXT_WARNING "Delete object files?\n");
+	PRINTLN_WARNING("Delete object files?");
 
-	printf(TEXT_WARNING "exec: " ANSI_BOLD ANSI_UNDERLINE "remove %s" ANSI_RESET " [Y/n] ", liu_config_object_dir);
+	PRINTF_WARNING("exec: " LIU_HIGHLIGHT("remove %s") " [Y/n] ", liu_config_object_dir);
 
 	int in = getc(stdin);
 	if (in == '\n' || in == 'y') {
@@ -415,8 +407,8 @@ void clean(void)
 		in = getc(stdin);
 	}
 
-	printf(TEXT_WARNING "Delete generated binaries?\n");
-	printf(TEXT_WARNING "exec: " ANSI_BOLD ANSI_UNDERLINE "remove %s" ANSI_RESET " [y/N] ", liu_config_binary_name);
+	PRINTLN_WARNING("Delete generated binaries?");
+	PRINTF_WARNING("exec: " LIU_HIGHLIGHT("remove %s") " [y/N] ", liu_config_binary_name);
 	in = getc(stdin);
 	if (in == 'y') {
 		remove(liu_config_binary_name);
@@ -426,7 +418,7 @@ void clean(void)
 int main(int argc, char** argv)
 {
 	if (argc == 1) {
-		printf(ANSI_RED ANSI_BOLD ANSI_UNDERLINE "No liu command" ANSI_RESET "\n\n");
+		printf(ANSI_RED LIU_HIGHLIGHT("No liu command") "\n\n");
 		liu_show_help(argv[0]);
 		return 0;
 	}
@@ -455,6 +447,6 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	printf(TEXT_FATAL "unknown command " ANSI_RED ANSI_BOLD ANSI_UNDERLINE "%s" ANSI_RESET "\n", cmd);
+	PRINTF_FATAL("unknown command " ANSI_RED LIU_HIGHLIGHT("%s") "\n", cmd);
 	return -1;
 }
